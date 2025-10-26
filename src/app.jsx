e10dcd53d74ddc4854b9f7b5ee7c7ca6c1ed7362
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-10-25 19:00:31 -0700

    dmca

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <main className="top-0 right-0 bottom-0 left-0 fixed flex flex-col justify-center items-center bg-[#313137] p-8 overflow-hidden text-white text-center">
+      <h1 className="mb-4 text-4xl">Fake Avatar Decorations</h1>
+      <p className="text-gray-300">The content of this site has been removed in compliance with a DMCA takedown notice.</p>
+    </main>
+  );
+}
